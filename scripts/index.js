f45cd8acad68d4158c07987191765ddad96163e7
Garalez <vladgalcev03@gmail.com>
2024-08-08 12:45:48 +0300

    preloader test

--- scripts/index.js
+++ scripts/index.js
@@ -1,107 +1,5 @@
 import { Swiper } from './swiper-bundle.min.js';
 
-const translationToShow = () => {
-  const storedLang = localStorage.getItem('storedLang');
-
-  const redirectToLanguagePage = lang => {
-    const pages = {
-      ru: 'index.html',
-      en: 'index-en.html',
-      uk: 'index-ua.html',
-    };
-    const page = pages[lang] || 'index.html';
-    localStorage.setItem('storedLang', lang);
-    window.location.href = page;
-  };
-
-  if (!storedLang) {
-    let userLang = navigator.language || navigator.userLanguage;
-    userLang = userLang.toLowerCase();
-
-    if (userLang.startsWith('ru')) {
-      redirectToLanguagePage('ru');
-    } else if (userLang.startsWith('en')) {
-      redirectToLanguagePage('en');
-    } else if (userLang.startsWith('uk')) {
-      redirectToLanguagePage('ua');
-    } else {
-      redirectToLanguagePage('en');
-    }
-  } else {
-    const currentUrl = window.location.href;
-    const langPages = {
-      ru: 'index.html',
-      en: 'index-en.html',
-      ua: 'index-ua.html',
-    };
-
-    if (!currentUrl.includes(langPages[storedLang])) {
-      window.location.href = langPages[storedLang];
-    }
-  }
-
-  document.querySelectorAll('.header__language-switcher-select').forEach(el => {
-    el.addEventListener('click', event => {
-      event.preventDefault();
-      const selectedLang = event.target.getAttribute('data-lang');
-      redirectToLanguagePage(selectedLang);
-    });
-  });
-};
-
-translationToShow();
-
-const usersSubmittingCounter = () => {
-  const updateProgress = percentage => {
-    const progressBar = document.querySelector('.percent-text');
-    const loaderBar = document.querySelector('.loader-bar');
-    loaderBar.style.width = `${percentage}%`;
-    progressBar.textContent = `${Math.round(percentage)}%`;
-  };
-
-  const simulateProgress = (progress = 0) => {
-    const maxProgress = 99;
-    const step = 5;
-    let requestId;
-
-    const incrementProgress = () => {
-      if (progress < maxProgress) {
-        progress += step;
-        updateProgress(progress);
-        requestId = requestAnimationFrame(incrementProgress);
-      }
-    };
-    requestId = requestAnimationFrame(incrementProgress);
-
-    return () => cancelAnimationFrame(requestId);
-  };
-
-  const stopProgressAnimation = simulateProgress();
-
-  const apiKey = 'AIzaSyDHJ0HulbJHVfvgmEH_8BHEE8hqD922ZTY';
-  const spreadsheetId = '1-IWrz95e2KX3bvdqUq3xs1Sh1rAIPMDWd7aik73pTco';
-  const range = 'Лист1!M2:M2';
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      stopProgressAnimation();
-      const value = data.values[0][0];
-      document.querySelectorAll('.counter').forEach(el => {
-        el.textContent = value;
-      });
-      updateProgress(100);
-    })
-    .catch(error => {
-      stopProgressAnimation();
-      console.error('Error fetching data:', error);
-      updateProgress(100);
-    });
-};
-
-usersSubmittingCounter();
-
 new Swiper('#reviews', {
   slidesPerView: 2,
   spaceBetween: 1,
@@ -119,6 +17,27 @@ new Swiper('#team', {
     delay: 3000,
   },
 });
+
+const usersSubmittingCounter = () => {
+  const apiKey = 'AIzaSyDHJ0HulbJHVfvgmEH_8BHEE8hqD922ZTY';
+  const spreadsheetId = '1-IWrz95e2KX3bvdqUq3xs1Sh1rAIPMDWd7aik73pTco';
+  const range = 'Лист1!M2:M2';
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const value = data.values[0][0];
+      document.querySelectorAll('.counter').forEach(el => {
+        el.textContent = value;
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+};
+
+usersSubmittingCounter();
 
 document.querySelectorAll('.open-modal').forEach(function (element) {
   element.addEventListener('click', function () {
@@ -292,44 +211,34 @@ document.querySelector('.footer__policy-button').addEventListener('click', () =>
   });
 });
 
-const preloaderAnimation = () => {
-  const blocks = [
-    { class: '.block1', effect: 'fade-down-left' },
-    { class: '.block2', effect: 'fade-down-right' },
-    { class: '.block3', effect: 'fade-down-right' },
-    { class: '.block4', effect: 'fade-up-left' },
-    { class: '.block5', effect: 'fade-up-right' },
-  ];
+const telegramButton = document.querySelectorAll('.telegram-button');
 
-  const animationDuration = 100;
-  const delayBetweenBlocks = 500;
-
-  blocks.forEach((block, index) => {
-    setTimeout(() => {
-      const element = document.querySelectorAll(block.class);
-      element.forEach(el => el.classList.add(block.effect));
-    }, index * delayBetweenBlocks);
+telegramButton.forEach(el => {
+  el.addEventListener('click', event => {
+    event.preventDefault();
+    const url = 'https://t.me/drop100f_bot?start=link_t9TbqYzkkA';
+    window.open(url, '_blank');
   });
+});
 
-  const totalDelay = (blocks.length - 1) * delayBetweenBlocks + animationDuration + 500;
+
+document.addEventListener('DOMContentLoaded', () => {
   const html = document.querySelector('html');
+  const body = document.querySelector('body');
   const preloader = document.querySelector('.preloader');
-  const preloaderGreetings = document.querySelectorAll('.main-title');
-  const whiteScreen = document.querySelector('.slide');
   const preloaderGreetingsImg = document.querySelector('.preloader__greetings-main-title-img');
-  const imgTopPart = document.querySelectorAll('.preloader__greetings-top');
-  const imgCenterPart = document.querySelectorAll('.preloader__greetings-center');
-  const imgBottomPart = document.querySelectorAll('.preloader__greetings-bottom');
+  const whiteScreen = document.querySelector('.slide');
 
-  setTimeout(() => {
-    preloaderGreetings.forEach(el => (el.style.display = 'none'));
-    preloaderGreetingsImg.style.display = 'flex';
-    imgTopPart.forEach(el => el.classList.add('shake-left'));
-    imgCenterPart.forEach(el => el.classList.add('shake-center-right'));
-    imgBottomPart.forEach(el => el.classList.add('shake-right'));
-  }, totalDelay);
+  const criticalImage = './assets/img/preloader/preloaderBg.png';
 
-  setTimeout(() => {
+  const preloadCriticalImage = src => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => (body.style.opacity = 1);
+    img.onerror = () => (body.style.opacity = 1);
+  };
+
+  const onPageLoad = () => {
     preloaderGreetingsImg.style.opacity = 0;
     whiteScreen.style.height = '100vh';
     setTimeout(() => {
@@ -339,17 +248,250 @@ const preloaderAnimation = () => {
         preloader.style.display = 'none';
       }, 500);
     }, 500);
-  }, totalDelay + 200);
-};
+  };
 
-preloaderAnimation();
+  document.querySelector('.preloader-screen').style.zIndex = '999999';
+  document.querySelector('.preloader-screen').style.opacity = '1';
 
-const telegramButton = document.querySelectorAll('.telegram-button');
+  const blocks = [
+    { class: '.block1', effect: 'fade-down-left' },
+    { class: '.block2', effect: 'fade-down-right' },
+    { class: '.block3', effect: 'fade-down-right' },
+    { class: '.block4', effect: 'fade-up-left' },
+    { class: '.block5', effect: 'fade-up-right' },
+  ];
 
-telegramButton.forEach(el => {
-  el.addEventListener('click', event => {
-    event.preventDefault();
-    const url = 'https://t.me/drop100f_bot?start=link_t9TbqYzkkA';
-    window.open(url, '_blank');
+  let currentIndex = 0;
+  let pageLoaded = false;
+  let animationComplete = false;
+
+  function showBlocks() {
+    const mainTitle = document.querySelector('.main-title');
+    const preloaderGreetingsImgTopPart = document.querySelectorAll('.preloader__greetings-top');
+    const preloaderGreetingsImgCenterPart = document.querySelectorAll(
+      '.preloader__greetings-center'
+    );
+    const preloaderGreetingsImgBottomPart = document.querySelectorAll(
+      '.preloader__greetings-bottom'
+    );
+
+    if (pageLoaded && animationComplete) {
+      return;
+    }
+    if (currentIndex === blocks.length) {
+      mainTitle.style.opacity = 0;
+      mainTitle.style.visibility = 'hidden';
+      mainTitle.style.height = '0';
+      blocks.forEach(block => {
+        const element = document.querySelector(block.class);
+        element.classList.remove(block.effect);
+        element.style.opacity = '';
+        element.style.transform = '';
+      });
+
+      currentIndex = 0;
+
+      preloaderGreetingsImg.style.opacity = 1;
+      preloaderGreetingsImg.style.visibility = 'visible';
+      preloaderGreetingsImg.style.height = '100%';
+
+      setTimeout(() => {
+        preloaderGreetingsImgTopPart.forEach(el => el.classList.add('shake-left'));
+        preloaderGreetingsImgCenterPart.forEach(el => el.classList.add('shake-center-right'));
+        preloaderGreetingsImgBottomPart.forEach(el => el.classList.add('shake-right'));
+
+        setTimeout(() => {
+          document.querySelector('.main-title').style.opacity = 1;
+          preloaderGreetingsImg.style.opacity = 0;
+          preloaderGreetingsImg.style.visibility = 'hidden';
+          preloaderGreetingsImg.style.height = '0';
+
+          preloaderGreetingsImgTopPart.forEach(el => el.classList.remove('shake-left'));
+          preloaderGreetingsImgCenterPart.forEach(el => el.classList.remove('shake-center-right'));
+          preloaderGreetingsImgBottomPart.forEach(el => el.classList.remove('shake-right'));
+          if (pageLoaded) {
+            mainTitle.style.opacity = 1;
+            mainTitle.style.visibility = 'visible';
+            mainTitle.style.height = '100%';
+            animationComplete = true;
+            onPageLoad();
+          } else {
+            showBlocks();
+          }
+        }, 100);
+      }, 100);
+    } else {
+      mainTitle.style.opacity = 1;
+      mainTitle.style.visibility = 'visible';
+      mainTitle.style.height = '100%';
+      const element = document.querySelector(blocks[currentIndex].class);
+      element.classList.add(blocks[currentIndex].effect);
+      currentIndex++;
+      setTimeout(showBlocks, 500);
+    }
+  }
+
+  const loaderBar = document.querySelector('.loader-bar');
+  const percentText = document.getElementById('percentText');
+
+  const logPageLoadTime = () => {
+    loaderBar.style.width = `${100}%`;
+    percentText.textContent = `${100}%`;
+    pageLoaded = true;
+  };
+
+  window.addEventListener('load', logPageLoadTime);
+  preloadCriticalImage(criticalImage);
+  showBlocks();
+
+  const allBackgroundImages = Array.from(document.querySelectorAll('*'))
+    .map(element => window.getComputedStyle(element).backgroundImage)
+    .filter(image => image !== 'none')
+    .map(image => image.replace(/url\(['"]?(.*?)['"]?\)/i, '$1'));
+
+  const allImgTags = Array.from(document.querySelectorAll('img')).map(img => img.src);
+
+  const allResources = [...new Set([...allBackgroundImages, ...allImgTags])];
+  const totalResources = allResources.length;
+
+  let loadedResources = 0;
+
+  const updateLoader = () => {
+    const percentage = Math.min(Math.round((loadedResources / totalResources) * 100), 100);
+
+    if (percentage >= 97) {
+      loaderBar.style.width = `${97}%`;
+      percentText.textContent = `${97}%`;
+    } else {
+      loaderBar.style.width = `${percentage}%`;
+      percentText.textContent = `${percentage}%`;
+    }
+  };
+
+  const resourceLoaded = src => {
+    if (loadedResources < totalResources) {
+      loadedResources++;
+      updateLoader();
+    }
+  };
+
+  const preloadImages = images => {
+    images.forEach(src => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => resourceLoaded(src);
+      img.onerror = () => resourceLoaded(src);
+    });
+  };
+
+  preloadImages(allResources);
+
+  const lazyBackgrounds = document.querySelectorAll('.lazy-bg');
+  const lazyPictures = document.querySelectorAll('.lazy-picture');
+  const lazyImages = document.querySelectorAll('.lazy-img');
+  const lazyImagesMobile = document.querySelectorAll('.lazy-img-mobile');
+
+  const lazyLoadBackgrounds = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const bg = entry.target;
+        const isMobile = window.matchMedia('(max-width: 590px)').matches;
+        const bgUrl = isMobile ? bg.getAttribute('data-bg-mobile') : bg.getAttribute('data-bg');
+        if (bgUrl) {
+          bg.style.backgroundImage = `url(${bgUrl})`;
+
+          const img = new Image();
+          img.src = bgUrl;
+          img.onload = () => resourceLoaded();
+          img.onerror = () => resourceLoaded();
+
+          observer.unobserve(bg);
+        }
+      }
+    });
+  };
+
+  const lazyLoadPictures = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const picture = entry.target;
+        const desktopSrc = picture.getAttribute('data-src-desktop');
+        const mobileSrc = picture.getAttribute('data-src-mobile');
+        const isMobile = window.matchMedia('(max-width: 590px)').matches;
+
+        if (desktopSrc && mobileSrc) {
+          if (isMobile) {
+            picture.querySelector('source[media="(max-width: 590px)"]').srcset = mobileSrc;
+            picture.querySelector('img').src = mobileSrc;
+          } else {
+            picture.querySelector('source[media="(min-width: 591px)"]').srcset = desktopSrc;
+            picture.querySelector('img').src = desktopSrc;
+          }
+
+          const img = new Image();
+          img.src = isMobile ? mobileSrc : desktopSrc;
+          img.onload = () => resourceLoaded();
+          img.onerror = () => resourceLoaded();
+
+          observer.unobserve(picture);
+        }
+      }
+    });
+  };
+
+  const lazyLoadImages = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const imgSrc = img.getAttribute('data-src');
+        if (imgSrc) {
+          img.src = imgSrc;
+          img.onload = () => resourceLoaded();
+          img.onerror = () => resourceLoaded();
+          observer.unobserve(img);
+        }
+      }
+    });
+  };
+
+  const lazyLoadImagesMobile = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && window.matchMedia('(max-width: 590px)').matches) {
+        const img = entry.target;
+        const imgSrc = img.getAttribute('data-src');
+        if (imgSrc) {
+          img.src = imgSrc;
+          img.onload = () => resourceLoaded();
+          img.onerror = () => resourceLoaded();
+          observer.unobserve(img);
+        }
+      }
+    });
+  };
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '500px',
+    threshold: 0.1,
+  };
+
+  const bgObserver = new IntersectionObserver(lazyLoadBackgrounds, observerOptions);
+  lazyBackgrounds.forEach(bg => {
+    bgObserver.observe(bg);
+  });
+
+  const pictureObserver = new IntersectionObserver(lazyLoadPictures, observerOptions);
+  lazyPictures.forEach(picture => {
+    pictureObserver.observe(picture);
+  });
+
+  const imgObserver = new IntersectionObserver(lazyLoadImages, observerOptions);
+  lazyImages.forEach(img => {
+    imgObserver.observe(img);
+  });
+
+  const imgMobileObserver = new IntersectionObserver(lazyLoadImagesMobile, observerOptions);
+  lazyImagesMobile.forEach(img => {
+    imgMobileObserver.observe(img);
   });
 });
